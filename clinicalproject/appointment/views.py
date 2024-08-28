682e7a8ce6c4d774589f6c081f78459c4848c22a
IvCAl <calivaivanalejandro@gmail.com>
2024-08-28 20:05:12 -0300

    paises appointment

--- clinicalproject/appointment/views.py
+++ clinicalproject/appointment/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from .models import Paises
+from django.views.decorators.csrf import csrf_exempt
+
 
 class PaisesCrud(View):
     def get(self, request):
