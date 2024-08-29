70473859c3e37bf1a0c3bec4b03b06b0b82ac9b6
headlightsdc <headlightsdc@protonmail.com>
2024-08-29 01:27:31 -0300

    bug fix authentication templates y creacion de app "portal". Eliminacion de urls y views appointment.

--- clinicalproject/authentication/urls.py
+++ clinicalproject/authentication/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.signin, name='signin'),
+    path('login/', views.signin, name='login'),
     path('signup/', views.signup, name='signup'),
 ]
