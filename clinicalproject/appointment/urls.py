48d2065e863ddf0a89701d7ee55f359dee9e28ca
headlightsdc <headlightsdc@protonmail.com>
2024-08-29 02:28:24 -0300

    Ejemplo de CRUD contactos en appointment funcional.

--- clinicalproject/appointment/urls.py
+++ clinicalproject/appointment/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('appointment/', views.appointment_view, name='appointment'),
+    path('contactos/', views.contactos_list, name='contactos_list'),
+    path('contactos/new/', views.contactos_create, name='contactos_create'),
+    path('contactos/edit/<int:pk>/', views.contactos_update, name='contactos_update'),
+    path('contactos/delete/<int:pk>/', views.contactos_delete, name='contactos_delete'),
 ]
--- clinicalproject/appointment/views.py
+++ clinicalproject/appointment/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Contactos
+from .forms import ContactosForm
 
 def appointment_view(request):
     return render(request, 'appointment.html')
+
+def contactos_list(request):
+    contactos = Contactos.objects.all()
+    return render(request, 'contactos_list.html', {'contactos': contactos})
+
+def contactos_create(request):
+    if request.method == 'POST':
+        form = ContactosForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contactos_list')
+    else:
+        form = ContactosForm()
+    return render(request, 'contactos_form.html', {'form': form})
+
+def contactos_update(request, pk):
+    contacto = get_object_or_404(Contactos, pk=pk)
+    if request.method == 'POST':
+        form = ContactosForm(request.POST, instance=contacto)
+        if form.is_valid():
+            form.save()
+            return redirect('contactos_list')
+    else:
+        form = ContactosForm(instance=contacto)
+    return render(request, 'contactos_form.html', {'form': form})
+
+def contactos_delete(request, pk):
+    contacto = get_object_or_404(Contactos, pk=pk)
+    if request.method == 'POST':
+        contacto.delete()
+        return redirect('contactos_list')
+    return render(request, 'contactos_confirm_delete.html', {'contacto': contacto})
